6250b72aba37183a3ec7e34af3d9ff3b38c7c68a
Aliaksandr Terashkou <aliaksandr.terashkou@itechart-group.com>
2020-09-04 14:02:38 +0300

    Initial commit

---
+++ tt2viewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tt2viewer
+//
+//  Created by nb-058-41b on 9/4/20.
+//  Copyright © 2020 DICE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
